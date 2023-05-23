bdc77371312f1161965ef73cbb52a6dad53ecbf7
dysleif <1904420563@qq.com>
2023-05-23 22:20:10 +0800

    这次提交你想要说的话

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btntemp=findViewById(R.id.button);
+        btntemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,nextActivity.class );
+                startActivity(intent);
+            }
+        });
+    }
+}
+
